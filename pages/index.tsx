162931d76697462704fd6090324f16fd6364c930
Reid Barber <reid@reidbarber.com>
2022-08-14 21:24:07 -0500

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,5 @@
-import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(false)
-
-  useEffect(() => {
-    setLoading(true)
-    fetch('https://stemplayer-next.vercel.app/api/tracks', {link: 'https://www.youtube.com/watch?v=oh2LWWORoiM'} as any)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data)
-        console.log(data);
-        setLoading(false)
-      })
-  }, [])
-
-  if (isLoading) return <p>Loading...</p>
-  if (!data) return <p>No data</p>
-  
-
   return (
     'tests'
   )
