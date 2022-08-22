27dfb75ce827184403aa12f3a7cbeaed024f0102
Reid Barber <reid@reidbarber.com>
2022-08-21 21:50:15 -0500

    add tracks to settings modal

--- components/SettingsModal/SettingsModal.tsx
+++ components/SettingsModal/SettingsModal.tsx
@@ -1,9 +1,12 @@
 import { Color } from "@react-types/color";
+import { Item } from "react-stately";
 import { useSettings } from "../../pages";
 import Button from "../Button/Button";
 import ColorWheel from "../ColorWheel/ColorWheel";
 import Modal from "../Modal/Modal";
 import { usePlayer } from "../Player/Player";
+import { Tabs } from "../Tabs/Tabs";
+import SelectTracks from "../WelcomeModal/SelectTracks";
 
 export default function SettingsModal(props: any) {
   let { onClose, closeButtonProps } = props;
@@ -17,39 +20,48 @@ export default function SettingsModal(props: any) {
 
   return (
     <Modal
-      title="Settings"
+      title="Options"
       isOpen
       onClose={onClose}
       isDismissable
       closeButtonProps={closeButtonProps}
     >
-      <Button onPress={() => window.location.reload()}>Restart Player</Button>
-      <Button onPress={() => toggleTheme()}>
-        Switch to {theme === "dark" ? "light" : "dark"} mode
-      </Button>
-      <Button onPress={() => toggleDynamicLights()}>
-        {dynamicLights ? "Disable" : "Enable"} Dynamic Lights
-      </Button>
-      <p>Dynamic lights may cause audio performance issues.</p>
-      <div style={{ marginBottom: 30 }}>
-        <div style={{ margin: 10 }}>Color 1 (Inside)</div>
-        <ColorWheel
-          value={colors[0].toString("hsl")}
-          onChange={onChangeColor1}
-          showPreview
-        />
-      </div>
-      <div>
-        <div style={{ margin: 10 }}>Color 2 (Outside)</div>
-        <ColorWheel
-          value={colors[1].toString("hsl")}
-          onChange={onChangeColor2}
-          showPreview
-        />
-        <p>More Coming Soon.</p>
-        <p>Not affiliated with YEEZY Tech or Kano.</p>
-        <p>Add to your home screen for an app-like experience.</p>
-      </div>
+      <Tabs aria-label="History of Ancient Rome">
+        <Item key="tracks" title="Tracks">
+          <SelectTracks onClose={onClose} />
+        </Item>
+        <Item key="settings" title="Settings">
+          <Button onPress={() => window.location.reload()}>
+            Restart Player
+          </Button>
+          <Button onPress={() => toggleTheme()}>
+            Switch to {theme === "dark" ? "light" : "dark"} mode
+          </Button>
+          <Button onPress={() => toggleDynamicLights()}>
+            {dynamicLights ? "Disable" : "Enable"} Dynamic Lights
+          </Button>
+          <p>Dynamic lights may cause audio performance issues.</p>
+          <div style={{ marginBottom: 30 }}>
+            <div style={{ margin: 10 }}>Color 1 (Inside)</div>
+            <ColorWheel
+              value={colors[0].toString("hsl")}
+              onChange={onChangeColor1}
+              showPreview
+            />
+          </div>
+          <div>
+            <div style={{ margin: 10 }}>Color 2 (Outside)</div>
+            <ColorWheel
+              value={colors[1].toString("hsl")}
+              onChange={onChangeColor2}
+              showPreview
+            />
+            <p>More Coming Soon.</p>
+            <p>Not affiliated with YEEZY Tech or Kano.</p>
+            <p>Add to your home screen for an app-like experience.</p>
+          </div>
+        </Item>
+      </Tabs>
     </Modal>
   );
 }
