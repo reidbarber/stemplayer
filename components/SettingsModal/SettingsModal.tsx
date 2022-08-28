c48243a386ddc9fc8a5dd2afbf8b97d82830d4fa
Reid Barber <reid@reidbarber.com>
2022-08-28 13:16:32 -0500

    add button to clear tracks

--- components/SettingsModal/SettingsModal.tsx
+++ components/SettingsModal/SettingsModal.tsx
@@ -18,6 +18,12 @@ export default function SettingsModal(props: any) {
   let onChangeColor1 = (val: Color) => setColors((prev) => [val, prev[1]]);
   let onChangeColor2 = (val: Color) => setColors((prev) => [prev[0], val]);
 
+  let clearTracks = () => {
+    if (typeof window !== "undefined" && localStorage) {
+      localStorage.removeItem("favorites");
+    }
+  };
+
   return (
     <Modal
       title="Options"
@@ -29,13 +35,13 @@ export default function SettingsModal(props: any) {
     >
       <Tabs aria-label="History of Ancient Rome">
         <Item key="tracks" title="Tracks">
-          <div style={{maxWidth: 400, margin: 'auto'}}>
-          <SelectTracks onClose={onClose} />
+          <div style={{ maxWidth: 400, margin: "auto" }}>
+            <SelectTracks onClose={onClose} />
           </div>
         </Item>
         <Item key="colors" title="Colors">
-          <div style={{display: 'flex', justifyContent: 'center'}}>
-            <div style={{margin: 10}}>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <div style={{ margin: 10 }}>
               <div style={{ margin: 10 }}>Color 1 (Inside)</div>
               <ColorWheel
                 value={colors[0].toString("hsl")}
@@ -43,7 +49,7 @@ export default function SettingsModal(props: any) {
                 showPreview
               />
             </div>
-            <div style={{margin: 10}}>
+            <div style={{ margin: 10 }}>
               <div style={{ margin: 10 }}>Color 2 (Outside)</div>
               <ColorWheel
                 value={colors[1].toString("hsl")}
@@ -54,21 +60,29 @@ export default function SettingsModal(props: any) {
           </div>
         </Item>
         <Item key="controls" title="Settings">
-          <div style={{display: 'flex', flexDirection: 'column', maxWidth: 300, margin: 'auto'}}>
-          <Button onPress={() => window.location.reload()}>
-            Restart Player
-          </Button>
-          <Button onPress={() => toggleTheme()}>
-            Switch to {theme === "dark" ? "light" : "dark"} mode
-          </Button>
-          <Button onPress={() => toggleDynamicLights()}>
-            {dynamicLights ? "Disable" : "Enable"} Dynamic Lights
-          </Button>
-          <p>Dynamic lights may cause audio performance issues.</p>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              maxWidth: 300,
+              margin: "auto",
+            }}
+          >
+            <Button onPress={() => window.location.reload()}>
+              Restart Player
+            </Button>
+            <Button onPress={() => toggleTheme()}>
+              Switch to {theme === "dark" ? "light" : "dark"} mode
+            </Button>
+            <Button onPress={() => toggleDynamicLights()}>
+              {dynamicLights ? "Disable" : "Enable"} Dynamic Lights
+            </Button>
+            <Button onPress={() => clearTracks()}>Clear Tracks</Button>
+            <p>Dynamic lights may cause audio performance issues.</p>
           </div>
         </Item>
         <Item key="about" title="about">
-        <div>
+          <div>
             <p>More Coming Soon.</p>
             <p>Not affiliated with YEEZY Tech or Kano.</p>
             <p>Add to your home screen for an app-like experience.</p>
