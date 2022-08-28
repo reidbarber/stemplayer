f2920e86b9c05a35310b2cf558ad6c9033015dd3
Reid Barber <reid@reidbarber.com>
2022-08-28 12:47:22 -0500

    fix meta scale

--- pages/_app.js
+++ pages/_app.js
@@ -45,10 +45,7 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Archivo:wght@500;600;700&family=Inter:wght@300;600&display=swap"
           rel="stylesheet"
         /> */}
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"
-        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
       </Head>
       <Component {...pageProps} />
     </SSRProvider>
