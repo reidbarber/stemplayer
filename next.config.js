ef3d7e520a901f78a0d51d27f40c7047505520f2
Reid Barber <reid@reidbarber.com>
2022-09-01 22:41:15 -0500

    disable react strict mode

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ['img.youtube.com'],
-  }
-}
+    domains: ["img.youtube.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
